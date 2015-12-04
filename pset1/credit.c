7ddaf54719d9ba29b2e66b2a14c0071f1d79c17d
ivanbrennan <ivan.brennan@gmail.com>
2015-12-03 22:23:13 -0500

    better credit

--- pset1/credit.c
+++ pset1/credit.c
@@ -2,6 +2,8 @@
 
 int valid(int digits[], int len);
 
+void printCardType(int digits[], int len);
+
 int main(void)
 {
 	long long number, balance;
@@ -12,7 +14,7 @@ int main(void)
 	printf("Number: ");
 	scanf(" %lld", &number);
 
-	// store digits
+	// parse digits
 	balance = number;
 	i = 0;
 	while (balance > 0) {
@@ -21,18 +23,9 @@ int main(void)
 		i++;
 	}
 
+	// print result
 	if (valid(digits, i)) {
-		head = (10 * digits[i-1]) + digits[i-2];
-
-		if ((i == 15) && (head == 34 || head == 37)) {
-			printf("AMEX\n");
-		} else if ((i ==16 ) && (51 <= head && head <= 55)) {
-			printf("MASTERCARD\n");
-		} else if (((i == 13) || (i == 16)) && (head / 10 == 4)) {
-			printf("VISA\n");
-		} else {
-			printf("INVALID\n");
-		}
+		printCardType(digits, i);
 	} else {
 		printf("INVALID\n");
 	}
@@ -54,4 +47,23 @@ int valid(int digits[], int len)
 	}
 
 	return (sum % 10 == 0);
+}
+
+void printCardType(int digits[], int len)
+{
+	int head;
+
+	head = (10 * digits[len - 1]) + digits[len - 2];
+
+	if ((len == 15) && (head == 34 || head == 37)) {
+		printf("AMEX\n");
+	} else if ((len == 16 ) && (51 <= head && head <= 55)) {
+		printf("MASTERCARD\n");
+	} else if (((len == 13) || (len == 16)) && (head / 10 == 4)) {
+		printf("VISA\n");
+	} else {
+		printf("INVALID\n");
+	}
+
+	return;
 }
