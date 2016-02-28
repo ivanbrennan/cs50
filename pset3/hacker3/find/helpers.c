796b0160579e4ec80a7bb3681d771dfcba6454e0
ivanbrennan <ivan.brennan@gmail.com>
2016-02-28 18:52:19 -0500

    radix sort wip

--- pset3/hacker3/find/helpers.c
+++ pset3/hacker3/find/helpers.c
@@ -8,13 +8,19 @@
  */
        
 #include <cs50.h>
+#include <math.h>
 
 #include "helpers.h"
 
+#define RADIX 10
+
+void countingSort(int values[], int n);
 int maxValue(int values[], int n);
 void zeroOut(unsigned int positions[], int positions_len);
 void buildHistogram(int values[], int n, unsigned int positions[]);
 void sortyByPosition(int values[], int n, unsigned int positions[], int positions_len);
+void radixSortLSB(int values[], int n);
+void clearAry(int ary[], int n);
 
 /**
  * Returns true if value is in array of n values, else false.
@@ -39,6 +45,11 @@ bool search(int value, int array[], int n)
  * Sorts array of n values.
  */
 void sort(int values[], int n)
+{
+	radixSortLSB(values, n);
+}
+
+void countingSort(int values[], int n)
 {
 	int positions_len = maxValue(values, n) + 1;
 	unsigned int positions[positions_len];
@@ -92,5 +103,50 @@ void sortyByPosition(int values[], int n, unsigned int positions[], int position
 		{
 			values[index++] = i;
 		}
+	}
+}
+
+void radixSortLSB(int values[], int n)
+{
+	int buckets[RADIX][n];
+
+	for (int i = 0; i < RADIX; i++)
+	{
+		clearAry(buckets[i], n);
+	}
+
+	int maxVal = maxValue(values, n);
+	int maxPow = (int) log10(maxVal);
+
+	for (int i = 0; i <= maxPow; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			int index = (values[j] / ((int) pow(RADIX, i))) % RADIX;
+			int k = 0;
+			while (buckets[index][k] != -1)
+			{
+				k++;
+			}
+			buckets[index][k] = values[j];
+		}
+
+		int v = 0;
+		for (int j = 0; j < RADIX; j++)
+		{
+			for (int k = 0; k < n && buckets[j][k] != -1; k++)
+			{
+				values[v++] = buckets[j][k];
+				buckets[j][k] = -1;
+			}
+		}
+	}
+}
+
+void clearAry(int ary[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		ary[i] = -1;
 	}
 }
