1da90cea9dfb3986a24e0db057624de931c3f73d
ivanbrennan <ivan.brennan@gmail.com>
2016-02-27 23:33:06 -0500

    less code

--- pset3/hacker3/find/helpers.c
+++ pset3/hacker3/find/helpers.c
@@ -14,10 +14,7 @@
 int maxValue(int values[], int n);
 void zeroOut(unsigned int positions[], int positions_len);
 void buildHistogram(int values[], int n, unsigned int positions[]);
-void setQueuePositions(unsigned int positions[], int positions_len);
-void sortValues(int values[], int n, unsigned int positions[]);
-void copyFromValues(int values[], int n, int copies[]);
-void insertValue(int value, unsigned int positions[], int values[]);
+void sortyByPosition(int values[], int n, unsigned int positions[], int positions_len);
 
 /**
  * Returns true if value is in array of n values, else false.
@@ -49,9 +46,7 @@ void sort(int values[], int n)
 
 	buildHistogram(values, n, positions);
 
-	setQueuePositions(positions, positions_len);
-
-	sortValues(values, n, positions);
+	sortyByPosition(values, n, positions, positions_len);
 
     return;
 }
@@ -87,43 +82,15 @@ void buildHistogram(int values[], int n, unsigned int positions[])
 	}
 }
 
-void setQueuePositions(unsigned int positions[], int positions_len)
+void sortyByPosition(int values[], int n, unsigned int positions[], int positions_len)
 {
-	int position = 0;
+	int index = 0;
 
 	for (int i = 0; i < positions_len; i++)
 	{
-		int entries = positions[i];
-		positions[i] = position;
-		position += entries;
+		for (int j = 0, entries = positions[i]; j < entries; j++)
+		{
+			values[index++] = i;
+		}
 	}
-}
-
-void sortValues(int values[], int n, unsigned int positions[])
-{
-	// copy original values to a new array so we can iterate
-	// over them while modifying the original array in place
-	int copies[n];
-	copyFromValues(values, n, copies);
-
-	for (int i = 0; i < n; i++)
-	{
-		insertValue(copies[i], positions, values);
-	}
-}
-
-void copyFromValues(int values[], int n, int copies[])
-{
-	for (int i = 0; i < n; i++)
-	{
-		copies[i] = values[i];
-	}
-}
-
-void insertValue(int value, unsigned int positions[], int values[])
-{
-	int position = positions[value];
-	positions[value]++;
-
-	values[position] = value;
 }
